c554235f71e89ef7ed5f9f555a751f4545196e5c
witalonrodrigues <witalonar@outlook.com>
2026-05-19 23:39:19 -0300

    Updates

---
+++ assets/send-CxJdB_Gh.js
@@ -0,0 +1,1 @@
+import{q as a}from"./index-DT06EDWl.js";const e=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],c=a("send",e);export{c as S};
